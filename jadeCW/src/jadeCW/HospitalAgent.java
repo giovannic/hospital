2afb904a2d977734213546fc15cf8af19812afa9
Adam Fiksen <af1410@ic.ac.uk>
2014-02-06 16:36:43 +0000

    setup receive appt request

--- jadeCW/src/jadeCW/HospitalAgent.java
+++ jadeCW/src/jadeCW/HospitalAgent.java
@@ -1,21 +1,36 @@
 package jadeCW;
 
+import jade.content.Concept;
+import jade.content.ContentElement;
+import jade.content.lang.Codec.CodecException;
+import jade.content.lang.sl.SLCodec;
+import jade.content.onto.OntologyException;
+import jade.content.onto.UngroundedException;
+import jade.content.onto.basic.Action;
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
 import jade.domain.FIPANames;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 
 
 @SuppressWarnings("serial")
 public class HospitalAgent extends Agent {
 	
 	private int available;
+	private AID[] takenSlots;
 	
 	@Override
 	protected void setup(){
-		available = Integer.parseInt(getArguments()[0].toString());
+		int appts = Integer.parseInt(getArguments()[0].toString());
+		available = appts;
+		takenSlots = new AID[appts];
+		addBehaviour(new AllocateAppointment(this));
 		registerAppointmentAgent("Hospital");
 	}
 	
@@ -26,6 +41,7 @@ public class HospitalAgent extends Agent {
 	  		ServiceDescription sd = new ServiceDescription();
 	  		sd.setName(serviceName);
 	  		sd.setType("allocate-appointments");
+			getContentManager().registerLanguage(new SLCodec(), FIPANames.ContentLanguage.FIPA_SL);
 	  		// Agents that want to use this service need to "know" the appointment-ontology
 			getContentManager().registerOntology(AppointmentOntology.getInstance());
 	  		// Agents that want to use this service need to "speak" the FIPA-SL language
@@ -37,5 +53,50 @@ public class HospitalAgent extends Agent {
 	  	catch (FIPAException fe) {
 	  		fe.printStackTrace();
 	  	}
+	}
+	
+	public class AllocateAppointment extends CyclicBehaviour {
+
+		public AllocateAppointment(Agent agent) {
+			super(agent);
+		}
+
+		public void action() {
+			ACLMessage msg = receive(MessageTemplate.MatchPerformative(ACLMessage.REQUEST));
+			if(msg != null) {
+				System.out.println("Agent "+getLocalName()+": REQUEST message received.");
+				ACLMessage reply = msg.createReply();
+				reply.addReceiver(msg.getSender());
+				
+				if(available <= 0){
+					reply.setPerformative(ACLMessage.REFUSE);
+				}
+				else {
+					int slot = -1;
+					try {
+						ContentElement content = getContentManager().extractContent(msg);
+						takenSlots[--available] = msg.getSender();
+						slot = available;
+						
+						
+					} catch (UngroundedException e) {
+						e.printStackTrace();
+					} catch (CodecException e) {
+						e.printStackTrace();
+					} catch (OntologyException e) {
+						e.printStackTrace();
+					}
+					if(slot >= 0) {
+						reply.setPerformative(ACLMessage.INFORM);
+						reply.setContent(Integer.toString(slot));
+					} else {
+						reply.setPerformative(ACLMessage.REFUSE);
+					}
+				}
+				send(reply);
+			} else {
+				block();
+			}
+		}	
 	}
 }
